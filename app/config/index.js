8b2ad8c238a015e1929851981514bbc7101caf10
hungb2105544 <hungb2105544@student.ctu.edu.vn>
2025-02-15 11:03:30 +0700

    Step 1 - Cai dat MongoDB vaf cai dat ham tro giup ket noi va lop dich vu truy xuat co so du lieu

--- app/config/index.js
+++ app/config/index.js
@@ -4,6 +4,9 @@ const config = {
     app: {
         port: process.env.PORT || 3000
     },
+    dp: {
+        uri: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/contactbook"
+    }
 };
 
 module.exports = config;
--- sever.js
+++ sever.js
@@ -1,7 +1,20 @@
 const app = require('./app');
 const config = require('./app/config'); 
+const MongoDB = require('./app/utils/mongodb.util');
 
-const PORT = config.app.port;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}.`);
-});   
+async function startServer(){
+    try{
+        await MongoDB.connect(config.dp.uri);
+        console.log('Connected to database successfully');
+
+        const PORT = config.app.port;
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}.`);
+        });
+    }catch(e){
+        console.log("Cannot connect to database", e);
+        process.exit();
+    }
+}
+
+startServer();
